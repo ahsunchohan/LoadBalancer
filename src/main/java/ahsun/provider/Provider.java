3527554c8c3e79724f950d6866d3b9be92a80a64
ahsunchohan <ahsunchohan@microsoft.com>
2022-06-26 16:52:51 +0100

    Complete step 2

--- src/main/java/ahsun/provider/Provider.java
+++ src/main/java/ahsun/provider/Provider.java
@@ -5,13 +5,22 @@ import java.util.UUID;
 public class Provider {
 
     private UUID uuid;
+    private int numRequests;
+    public final static int maxRequests = 10;
 
     public Provider(){
         this.uuid = UUID.randomUUID();
     }
 
     public String get(){
-        return this.uuid.toString();
+        if (numRequests < Provider.maxRequests) {
+            numRequests++;
+            return this.uuid.toString();
+        }else if (numRequests == Provider.maxRequests) {
+            return "MaxRequestsReached";
+        }else{
+            return null;
+        }        
     }
 
     public UUID getUuid() {
@@ -20,6 +29,18 @@ public class Provider {
 
     public void setUuid(UUID uuid) {
 		this.uuid = uuid;
+	}
+
+    public int getNumRequests() {
+		return numRequests;
+	}
+
+	public void setNumRequests(int numRequests) {
+		this.numRequests = numRequests;
+	}
+	
+	public void addRequest() {
+		this.numRequests++;
 	}
     
 }
